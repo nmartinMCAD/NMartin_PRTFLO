e7103601e9248e049ca3bf59f4336320bc0ba802
nmartinMCAD <nmartin@mcad.edu>
2017-11-28 18:52:53 -0700

    Penultimate Commit
    All that’s left is to consolidate my jQuery

--- PUBLIC/js/main.js
+++ PUBLIC/js/main.js
@@ -35,38 +35,13 @@ $(function() {
 });
 
 $(function() {
-	$("h3#blogButton").click(function() {
-		$("body").removeClass();
-		$("body").addClass("blogSection");
-	});
-});
-
-$(function() {
 	$(".siteSection i.fa-times").click(function() {
 		$("body").removeClass();
 	});
 });
 
 
-
 //Show more project info
-
-/*
-var moreInfo = "<i class='fa fa-plus' aria-hidden='true'></i><h3>more info</h3>";
-var lessInfo = "<i class='fa fa-minus' aria-hidden='true'></i><h3>less info</h3>";
-
-$("div.showMore").click(function (evt) {
-	  $(this).next().slideToggle("slow");
-		$(this).html(lessInfo);
-		$(this).attr("id", "showLess");
-});
-
-$("div#showLess").click(function (evt) {
-		$(this).html(moreInfo);
-		$(this).removeAttr("id");
-})
-
-*/
 
 $("div.showMore").click(function (evt) {
 	  $(this).next().slideToggle("slow");
@@ -74,16 +49,3 @@ $("div.showMore").click(function (evt) {
 			return text === "more info" ? "less info" : "more info";
 		})
 });
-
-
-/*
-$("div.showLess").click(function (evt) {
-	  $(this).parent().slideToggle("slow");
-});
-*/
-
-/*
-function onOpen () {
-	$(".projectInfoBox1").fadeIn(200);
-}
-*/
