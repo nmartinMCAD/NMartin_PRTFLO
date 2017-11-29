2e5f1a0900c18b731b66db269c1d91453a03456a
nmartinMCAD <nmartin@mcad.edu>
2017-11-29 14:54:10 -0700

    FINAL COMMIT (for now)
    This is my Final Commit for my mid-program review at MCAD.

--- PUBLIC/js/main.js
+++ PUBLIC/js/main.js
@@ -20,12 +20,17 @@ $(function() {
 	});
 });
 
+
+/* Event currently disabled until section is added
+
 $(function() {
 	$("h3#buttonFour").click(function() {
 		$("body").removeClass();
 		$("body").addClass("sectionFour");
 	});
 });
+
+*/
 
 $(function() {
 	$("h3#aboutButton").click(function() {
@@ -49,3 +54,27 @@ $("div.showMore").click(function (evt) {
 			return text === "more info" ? "less info" : "more info";
 		})
 });
+
+
+
+
+/* Code consolidation WIP
+
+$(".sectionButton").click(function (evt) {
+	$(".siteSection").hide();
+	$(".worktypebuttons h3").css({
+  "text-align" : "left",
+	"padding-left" : "1.5rem",
+	"width" : "100%" ,
+	"font-size": "3vw",})
+  $("#" + $(this).data("rel")).show();
+});
+
+$(function() {
+	$(".siteSection i.fa-times").click(function() {
+		$(".siteSection").hide();
+		$(".worktypebuttons h3")............
+	});
+});
+
+*/
